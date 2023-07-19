672be2be23d28d95536267279a64e984ad27c2fa
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-07-19 13:25:00 +0300

    Added DI

--- SQLApp/Program.cs
+++ SQLApp/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SQLApp
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SQLApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(app =>
+    {
+        app.SetBasePath(Directory.GetCurrentDirectory());
+        app.AddJsonFile("appsettings.json");
+    })
+    .ConfigureServices((_, services) =>
+    {
+        services.Configure<Settings>(_.Configuration.GetSection("Settings"));
+        services.AddScoped(typeof(INumberGenerator), typeof(NumberGenerator));
+        services.AddTransient(typeof(IUserInteractionReader), typeof(ConsoleReader));
+        services.AddTransient(typeof(IUserInteractionWriter), typeof(ConsoleWriter));
+        //services.AddSingleton(typeof(IGame), typeof(Game));
+        services.AddSingleton(typeof(IGameNotification), typeof(GameCore));
+    })
+    .Build();
+            var game = host.Services.GetService<IGameNotification>() ?? throw new ArgumentNullException("Game");
+
         }
     }
 }
